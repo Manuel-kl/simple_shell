0119bf3c6d5dc837dd0746327452d50271d36881
Langat Emmanuel <mannuehkipkirui@gmail.com>
2023-02-22 20:29:01 +0300

    simple shell task

--- main.c
+++ main.c
@@ -1,25 +1,45 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - shell proyect
- * @ac: count arguments
- * @av: store arguments
- * @env: variable envirionment system
- * Return: estatus_exit value;
- **/
-
-int main(int ac, char *av[], char **env)
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	int count_exe = 1, status_exit = 0;
-	(void)ac;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	signal(SIGINT, _sighandler);
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-	if (ac > 1)
+	if (ac == 2)
 	{
-		status_exit = non_interact(av);
-		return (status_exit);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	status_exit = interactive(av, count_exe, env);
-	return (status_exit);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
+
